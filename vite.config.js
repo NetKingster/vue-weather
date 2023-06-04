7b76cbc29767f8e45b1ac0189c085dc3598e9dcb
Sherlee <sherlee845@hotmail.com>
2023-06-04 11:46:11 +0800

    Deployed website

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    base: '/deploying-vite-project-example',
+    base: '/vue-weather',
     plugins: [vue()]
 })
